1e8b10cff8676fd3bbc67987cad33d3ebb48b9f0
Priyanshu Singh <reveur.guy99@gmail.com>
2023-02-11 17:18:49 +0530

    minor changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -28,7 +28,7 @@ Card.Link = function CardLink({ children, ...props }) {
     <>
       <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-100 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/80 sm:-inset-x-6 sm:rounded-2xl" />
       <Link {...props}>
-        <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl" />
+        <span className="absolute -inset-y-6 -inset-x-4 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
       </Link>
     </>
