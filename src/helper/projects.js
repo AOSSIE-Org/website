323023b185b5eb28f8528325bd9fcaede4529231
Chandan S Gowda <chandansuresh007@gmail.com>
2024-02-05 16:19:52 +0000

    Update projects.js

--- src/helper/projects.js
+++ src/helper/projects.js
@@ -1,66 +1,62 @@
 import DefaultLogo from '@/images/default.png'
-import AgoraLogo from '@/images/Agora.png';
-import CarbonLogo from '@/images/Carbon.png';
-import monumentoLogo from '@/images/monumento.png';
-import scholarLogo from '@/images/scholar.png';
-import SSSLogo from '@/images/SSS.png';
+import AgoraLogo from '@/images/Agora.png'
+import SSSLogo from '@/images/SSS.png'
+import ResonateLogo from '@/images/Resonate.png'
+
+// data
 
 const projects = [
-    {
-      name: 'Agora Blockchain',
-      description:
-        'There isn’t just one voting algorithm, there’s hundreds. Therefore, we developed Agora to allow for democratic elections using any algorithm.',
-      link: { href: 'https://gitlab.com/aossie/Agora', label: 'Agora' },
-      logo: AgoraLogo
+  {
+    name: 'Agora Blockchain',
+    description:
+      'There isn’t just one voting algorithm, there’s hundreds. Therefore, we developed Agora to allow for democratic elections using any algorithm.',
+    link: {
+      href: 'https://github.com/AOSSIE-Org/Agora-Blockchain',
+      label: 'Agora',
     },
-    {
-      name: 'Agora Vote Android',
-      description:
-        'This application uses Agora Web API as backend application. It allows for elections to be held by using multiple algorithms such as Oklahoma, RangeVoting, RankedPairs, Satisfaction Approval Voting.',
-        link: { href: 'https://gitlab.com/aossie/agora-android', label: 'Agora-Android' },
-      logo: AgoraLogo
+    logo: AgoraLogo,
+  },
+  {
+    name: 'Agora Vote Android',
+    description:
+      'This application uses Agora Web API as backend application. It allows for elections to be held by using multiple algorithms such as Oklahoma, RangeVoting, RankedPairs, Satisfaction Approval Voting.',
+    link: {
+      href: 'https://gitlab.com/aossie/agora-android',
+      label: 'Agora-Android',
     },
-    {
-      name:'EduAid',
-      description:
-        'A tool that can auto-generate short quizzes on the basis of the content provided.',
-        link: { href: '#', label: 'EduAid' },
-      logo: DefaultLogo
-    },
-    {
-      name:  'Monumento',
-      description:
-        'Monumento is a social media for sharing landmarks, visited places and visualizing their 3D models right from a mobile device ...',
-        link: { href: 'https://gitlab.com/aossie/monumento', label: 'Monumento' },
-      logo: monumentoLogo
-    },
-    {
-      name: 'OpenChat',
+    logo: AgoraLogo,
+  },
+  {
+    name: 'EduAid',
+    description:
+      'An online tool that can generate short quizzes on input educational content can be of great use to teachers and students alike as it can help retain important information, frame questions and quickly revise large chunks of content.',
+    link: { href: 'https://github.com/AOSSIE-Org/EduAid', label: 'EduAid' },
+    logo: DefaultLogo,
+  },
+  {
+    name: 'OpenChat',
     description:
       'a decentralised platform for secure and private messaging and file sharing built on top of blockchain',
-      link: { href: '#', label: 'OpenChat' },
-      logo: DefaultLogo
-    },
-    {
-      name: 'P2P Messaging- Flutter',
-      description:
-        'Chat application for Android and IOS in flutter/dart which sends messages Using Bluetooth & wifi-direct ...',
-        link: { href: 'https://gitlab.com/aossie/p2p-messaging-flutter', label: 'P2P-Messaging-Flutter' },
-      logo: DefaultLogo
-    },
-    {
-      name: 'Resonate',
-    description: 'a social voice platform',
-    link: { href: '#', label: 'Resonate' },
-      logo: DefaultLogo
-    },
-    {
-      name: 'Social Street Smart',
+    link: { href: '#', label: 'OpenChat' },
+    logo: DefaultLogo,
+  },
+  {
+    name: 'Resonate',
+    description:
+      'With the rising popularity of social voice platforms such as Clubhouse, it is high time for an Open Source alternative. A platform like this would not only enhance credibility within the open-source community but also attract more users and foster growth.  ',
+    link: { href: 'https://github.com/AOSSIE-Org/Resonate', label: 'Resonate' },
+    logo: ResonateLogo,
+  },
+  {
+    name: 'Social Street Smart',
     description:
       'With the adevent of Internet, the problems faced by the people have also grown. These include abusive languages.',
-      link: { href: 'https://gitlab.com/aossie/social-street-smart', label: 'Social-Street-Smart' },
-      logo: SSSLogo
+    link: {
+      href: 'https://github.com/AOSSIE-Org/Social-Street-Smart',
+      label: 'Social-Street-Smart',
     },
-  ]
+    logo: SSSLogo,
+  },
+]
 
 export default projects
