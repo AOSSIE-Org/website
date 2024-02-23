61ec8025d27d2e653145e6b70a6287b757b3fbe5
rishan <iamrmrishan@gmail.com>
2024-02-23 14:52:06 +0000

    improvement:Update project ideas list item widget

--- src/pages/ideas/index.jsx
+++ src/pages/ideas/index.jsx
@@ -4,15 +4,44 @@ import Link from 'next/link'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
 import { getAllIdeas } from '@/helper/getAllIdeas'
+import { limitCharacters } from '@/helper/limitCharacters'
 
 function Article({ article }) {
   return (
     <article className="mt-5 sm:mt-0 md:grid md:grid-flow-col md:grid-cols-4 md:items-baseline">
       <Card className="md:col-span-3">
-        <Card.Title href={`/ideas/2024/${article.slug}`}>
-          {article.title}
-        </Card.Title>
-        <Card.Description>{article.description}</Card.Description>
+        <div className="flex items-center justify-between gap-5">
+          <Card.Title href={`/ideas/2024/${article.slug}`}>
+            {article.title}
+          </Card.Title>
+          <Card.Tags>
+            {article.projectDuration ? article.projectDuration : 'TBD'}
+          </Card.Tags>
+          <Card.Tags>
+            {article.difficulty ? article.difficulty : 'TBD'}
+          </Card.Tags>
+        </div>
+        <Card.Description>
+          {limitCharacters(article.description, 350)}
+        </Card.Description>
+        <p className="text-md font-mono font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+          Expected outcomes
+        </p>
+        <Card.Info>
+          {article.outcomes ? limitCharacters(article.outcomes, 100) : 'TBD'}
+        </Card.Info>
+        <p className="text-md font-mono font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+          Skills
+        </p>
+        <Card.Info>
+          {article.skills ? limitCharacters(article.skills, 100) : 'TBD'}
+        </Card.Info>
+        <p className="text-md font-mono font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+          Mentors
+        </p>
+        <Card.Info>
+          {article.mentors ? limitCharacters(article.mentors, 50) : 'TBD'}
+        </Card.Info>
         <Card.Cta>Know More</Card.Cta>
       </Card>
     </article>
@@ -40,7 +69,7 @@ export default function Ideas({ articles }) {
         </div>
         <Container.Inner>
           <div className="mt-10 flex justify-center sm:mt-20">
-            <div className="grid gap-6 gap-y-8 md:grid-flow-col md:grid-rows-4">
+            <div className="grid gap-6 gap-y-8 md:grid-cols-2 md:grid-rows-4">
               {articles.map((article) => (
                 <Article key={article.slug} article={article} />
               ))}
