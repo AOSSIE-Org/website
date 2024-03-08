455411bda9aa45d3506ad3565e329f0d6fb675f0
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2024-03-08 10:22:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import rehypePrism from '@mapbox/rehype-prism'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
   experimental: {
