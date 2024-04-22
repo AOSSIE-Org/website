e135bd7e77a9c227c1385b1ae69bd62fbab3439d
Sagar Tumadiya <sagtumad@publicisgroupe.net>
2024-04-22 14:59:21 +0530

    added dynamic date feature to the footer and fixed the compiler error caused by nested tag in Banner.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ export function Footer() {
                 <NavLink href="/apply">Apply</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500 font-mono">
-                &copy; 2016-2023 AOSSIE. All rights reserved.
+                &copy; 2016-{new Date().getFullYear()} AOSSIE. All rights reserved.
               </p>
               <div className="flex gap-6">
                 <Link aria-label="Contact by Mail" className=' text-zinc-400 hover:text-[#00843D] dark:text-zinc-400 dark:hover:text-yellow-400 transition' href='mailto:aossie.oss@gmail.com'>
