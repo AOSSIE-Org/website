e0d97b7a859a6a0b4d671acc0cee9fa5524f5795
Ashwin Saklecha <ashwinsaklecha2004@gmail.com>
2024-08-04 22:05:14 +0530

    Edited the copyright year

--- src/pages/ideas/index.jsx
+++ src/pages/ideas/index.jsx
@@ -49,7 +49,7 @@ export default function Ideas({ articles }) {
           <div className="mt-16 text-center">
             <Link
               className="group order-2 mx-auto items-center overflow-hidden rounded-lg bg-zinc-800 px-8 py-3 text-white focus:outline-none dark:bg-white dark:text-black"
-              href="/ideas/2023"
+              href="/ideas/2024 "
             >
               <span className="text-center font-mono font-semibold">
                 View 2024 Idea List
