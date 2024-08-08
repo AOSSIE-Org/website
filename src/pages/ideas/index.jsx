b0bb4aa910820097568fbcc348e254df9037c1a8
its-harshitgoel <its.harshitgoel@gmail.com>
2024-08-08 21:24:10 +0530

    Fixed Hover Effect

--- src/pages/ideas/index.jsx
+++ src/pages/ideas/index.jsx
@@ -63,7 +63,7 @@ export default function Ideas({ articles }) {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3 "
                   />
                 </svg>
               </span>
