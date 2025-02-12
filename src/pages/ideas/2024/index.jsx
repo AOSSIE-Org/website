b4e4b16f98b724559453d71520c73506720fc975
Aarush-Acharya <e21cseu0041@bennett.edu.in>
2025-02-12 19:34:23 +0530

    feat: added new idea list for 2025

---
+++ src/pages/ideas/2024/index.jsx
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { Card } from '@/components/Card'
+import { Container } from '@/components/Container'
+import { getAllIdeas } from '@/helper/getAllIdeas2024'
+
+function Article({ article }) {
+  return (
+    <article className="md:grid md:grid-cols-4 md:grid-flow-col md:items-baseline sm:mt-0 mt-5">
+      <Card className="md:col-span-3">
+        <Card.Title href={`/ideas/2023/${article.slug}`}>
+          {article.title}
+        </Card.Title>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read More</Card.Cta>
+      </Card>
+    </article>
+  )
+}
+
+export default function Ideas({ articles }) {
+  return (
+    <>
+      <Head>
+        <title>
+          Idea List
+        </title>
+        <meta
+          name="description"
+          content="Idea List for GSOC 2023"
+        />
+      </Head>
+      <Container className="mt-20 mb-28">
+        <div className="flex justify-center items-center w-full">
+        <p className="font-mono text-lg leading-7 text-zinc-600 dark:text-zinc-400">
+           AOSSIE&apos;s{' '}
+            <b>Idea List</b>. for {' '}
+            <b>Google Summer of Code 2024</b>
+          </p>
+        </div>
+        <Container.Inner>
+          <div className="mt-10 sm:mt-20 flex justify-center">
+            <div className="grid gap-6 gap-y-20 md:grid-cols-3">
+              {articles.map((article) => (
+                <Article key={article.slug} article={article} />
+              ))}
+            </div>
+          </div>
+          <div className='text-center mt-16'>
+            <Link className="mx-auto order-2 group rounded-lg items-center overflow-hidden bg-zinc-800 dark:bg-white px-8 py-3 dark:text-black text-white focus:outline-none" href="/ideas">
+              <span className="font-mono font-semibold text-center">
+                Go Back
+              </span>
+            </Link>
+          </div>
+        </Container.Inner>
+      </Container>
+    </>
+    )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      articles: (await getAllIdeas()).map(({ component, ...meta }) => meta),
+    },
+  }
+}
--- src/pages/ideas/index.jsx
+++ src/pages/ideas/index.jsx
@@ -9,7 +9,7 @@ function Article({ article }) {
   return (
     <article className="mt-5 sm:mt-0 md:grid md:grid-flow-col md:grid-cols-4 md:items-baseline">
       <Card className="md:col-span-3">
-        <Card.Title href={`/ideas/2024/${article.slug}`}>
+        <Card.Title href={`/ideas/2025/${article.slug}`}>
           {article.title}
         </Card.Title>
         <Card.Description>{article.description}</Card.Description>
@@ -27,20 +27,19 @@ export default function Ideas({ articles }) {
         <meta name="description" content="Idea List for GSOC" />
       </Head>
       <Container className="mt-20 mb-28">
-        <div className="">
-          <p className="font-mono text-lg leading-7 text-zinc-600 dark:text-zinc-400">
+        <p className="font-mono text-lg leading-7 text-zinc-600 dark:text-zinc-400">
             Explore the world of open-source possibilities with AOSSIE&apos;s{' '}
-            <b>Idea List</b>. As part of Google Summer of Code, we offer
-            a unique opportunity for developers to explore new ideas,  a wide
+            <b>Idea List</b>. As part of {' '}
+            <b>Google Summer of Code 2025</b>, we offer
+            a unique opportunity for developers to explore new ideas, a wide
             variety of projects for developers to choose from and contribute to.
             From developing new features to fixing critical bugs, our idea list
             is your go-to destination for finding your next big project and
             kickstart your open-source journey.
-          </p>
-        </div>
+        </p>
         <Container.Inner>
           <div className="mt-10 flex justify-center sm:mt-20">
-            <div className="grid gap-6 gap-y-8 md:grid-flow-col md:grid-rows-4">
+            <div className="grid gap-6 gap-y-20 md:grid-cols-3">
               {articles.map((article) => (
                 <Article key={article.slug} article={article} />
               ))}
@@ -49,10 +48,10 @@ export default function Ideas({ articles }) {
           <div className="mt-16 text-center">
             <Link
               className="group order-2 mx-auto items-center overflow-hidden rounded-lg bg-zinc-800 px-8 py-3 text-white focus:outline-none dark:bg-white dark:text-black"
-              href="/ideas/2023"
+              href="/ideas/2024"
             >
               <span className="text-center font-mono font-semibold">
-                View 2023 Idea List
+                View 2024 Idea List
               </span>
             </Link>
           </div>
