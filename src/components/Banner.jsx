955d4e2a5a038aa7a888be62300a0c92c68aebca
ananyapandey9895 <ananya.pandey@adypu.edu.in>
2025-02-27 19:54:26 +0530

    Added Hover Effect to "Apply to GSoC with AOSSIE"
    By fixing this issue, I have introduced hover effect to "Apply to GSoC with AOSSIE" button on main page . This will help in creating a better UI to front page. When the User will click on "Apply to GSoC with AOSSIE" button , the button will pop-up.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -18,7 +18,7 @@ export function Banner() {
             </p>
             <div className="mt-5">
               <Link href="/apply" legacyBehavior>
-                <a className="group relative rounded-lg inline-flex items-center overflow-hidden bg-white dark:bg-black px-8 py-3 text-black dark:text-white focus:outline-none font-mono font-semibold">
+                <a className="group relative rounded-lg inline-flex items-center overflow-hidden bg-white dark:bg-black px-8 py-3 text-black dark:text-white focus:outline-none font-mono font-semibold transition-transform duration-300 ease-in-out hover:scale-110">
                   Apply to GSoC with AOSSIE
                 </a>
               </Link>
