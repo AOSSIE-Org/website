3835e721c88313768823659e4511d59212e810e4
shreyashap <107754587+shreyashap@users.noreply.github.com>
2025-03-01 17:33:43 +0530

    Fix image misalignment

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -13,8 +13,8 @@ export default function About() {
       </Head>
       <Container className="mt-16 sm:mt-32">
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-          <div className="lg:pl-20">
-            <div className="max-w-xs px-2.5 lg:max-w-none">
+          <div className="flex items-center justify-center lg:pl-20">
+            <div className="max-w-xs transform px-2.5 transition-transform duration-300 ease-in-out hover:rotate-6 hover:scale-110 hover:shadow-2xl hover:shadow-zinc-900/40 lg:max-w-none">
               <Image
                 src={BlackLogo}
                 alt="AOSSIE Logo"
