fdd5d0efc4939676abe13c7dcc0f7a39f9333869
shiavam kumar <maishivamhoo@gmail.com>
2025-03-26 22:36:05 +0530

    twiter icon changes to X icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,9 @@ import { Container } from '@/components/Container'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faDiscord, faGithub, faGitlab, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+
+
 
 function NavLink({ href, children }) {
   return (
@@ -43,9 +46,14 @@ export function Footer() {
                 <Link aria-label="Join on Discord" className=' text-zinc-400 hover:text-[#00843D] dark:text-zinc-400 dark:hover:text-yellow-400 transition' href='https://discord.gg/hjUhu33uAn'>
                   <FontAwesomeIcon icon={faDiscord} size='xl' />
                 </Link>
-                <Link aria-label="Follow on Twitter" className=' text-zinc-400 hover:text-[#00843D] dark:text-zinc-400 dark:hover:text-yellow-400 transition' href='https://twitter.com/aossie_org'>
-                  <FontAwesomeIcon icon={faTwitter} size='xl' />
-                </Link>
+                <Link
+  aria-label="Follow on X"
+  className="text-zinc-400 hover:text-[#00843D] dark:text-zinc-400 dark:hover:text-yellow-400 transition"
+  href="https://twitter.com/aossie_org"
+>
+  <FontAwesomeIcon icon={faXTwitter} size="xl" />
+</Link>
+
                 <p className="text-sm text-zinc-400 dark:text-zinc-500 font-mono">
                 &copy; 2016-2025 AOSSIE. All rights reserved.
               </p>
