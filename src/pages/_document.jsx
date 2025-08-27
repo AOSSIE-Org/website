c7f89577d6312a48ea0d63873c497f3e9d5b7e9d
Shiven <shivengaria@gmail.com>
2025-08-28 00:26:37 +0530

    Fix Custom Document Export and Inject Dark Mode Script

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,60 +1,46 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 const modeScript = `
-  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-
-  updateMode()
-  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
-  window.addEventListener('storage', updateModeWithoutTransitions)
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
   function updateMode() {
-    let isSystemDarkMode = darkModeMediaQuery.matches
-    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+    const isSystemDarkMode = darkModeMediaQuery.matches;
+    const isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode);
 
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
+    document.documentElement.classList.toggle('dark', isDarkMode);
 
     if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode
+      delete window.localStorage.isDarkMode;
     }
   }
 
   function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
+    document.documentElement.classList.add('[&_*]:!transition-none');
     window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
+      document.documentElement.classList.remove('[&_*]:!transition-none');
+    }, 0);
   }
 
   function updateModeWithoutTransitions() {
-    disableTransitionsTemporarily()
-    updateMode()
+    disableTransitionsTemporarily();
+    updateMode();
   }
-`
+
+  updateMode();
+  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions);
+  window.addEventListener('storage', updateModeWithoutTransitions);
+`;
 
 export default function Document() {
   return (
-    <Html className="h-full antialiased" lang="en">
+    <Html lang="en">
       <Head>
         <script dangerouslySetInnerHTML={{ __html: modeScript }} />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.xml`}
-        />
-        <link
-          rel="alternate"
-          type="application/feed+json"
-          href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
-        />
       </Head>
-      <body className="flex h-full flex-col bg-zinc-100 dark:bg-black">
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
