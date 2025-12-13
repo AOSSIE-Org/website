1445f101fec34ce4831db861c965874a85e40cc3
clOUdy <riyanshverma@jklu.edu.in>
2025-12-13 17:16:42 +0530

    remove unused NavLink component and related links from Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faDiscord, faGithub, faGitlab, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-function NavLink({ href, children }) {
-  return (
-    <Link
-      href={href}
-      className="transition hover:text-[#00843D] dark:hover:text-yellow-400"
-    >
-      {children}
-    </Link>
-  )
-}
-
 export function Footer() {
   return (
     <footer className="mt-16">
@@ -23,12 +12,6 @@ export function Footer() {
         <div className="pt-10 pb-10">
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <div className="flex gap-5 text-md font-semibold font-mono text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/about">About</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/ideas">Ideas</NavLink>
-                <NavLink href="/apply">Apply</NavLink>
-              </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500 font-mono">
                 &copy; 2016-2025 AOSSIE. All rights reserved.
               </p>
