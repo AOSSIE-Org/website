ae50896dd7c90a35f781ec31101c192c1ae7aa64
Muskan Kumari <muskankumari7371039@gmail.com>
2025-12-28 10:38:05 +0530

    Fix overlapping cards on tablet screen sizes

--- src/components/CardEffect.jsx
+++ src/components/CardEffect.jsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 
 export function CardEffect({heading, content, logo}) {
     return (
-        <a className="group relative block h-[22rem] max-lg:w-72 max-xl:w-60 w-72 cursor-pointer">
+        <a className="group relative block h-auto sm:w-72 md:w-full lg:w-72 cursor-pointer">
+
             {/* <span className="absolute inset-0 border-2 rounded-lg border-dashed border-black dark:border-zinc-300"></span> */}
 
             {/* <div className="relative flex h-full transform items-end border-4 rounded-lg border-black dark:border-zinc-300 bg-transparent dark:bg-transparent transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2"> */}
@@ -18,8 +19,11 @@ export function CardEffect({heading, content, logo}) {
                     />
                     <h2 className="ml-0 leading-9 text-4xl text-center flex font-extrabold justify-center font-mono text-[#00843D] dark:text-yellow-400">{heading}</h2>
                 </div>
-                <div className="absolute self-center pr-6 lg:scale-90 lg:pb-0 lg:pl-3 lg:pr-0 xl:pl-0 md:p-0 md:scale-95 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 dark:text-zinc-300">
-                    <p className="mt-4 font-mono sm:w-100 w-52">{content}</p>
+                <div className=" md:relative 
+                md:opacity-100 
+                lg:absolute lg:opacity-0 lg:group-hover:relative lg:group-hover:opacity-100
+                self-center pr-6 transition-opacity dark:text-zinc-300">
+                    <p className="mt-4 font-mono w-full sm:max-w-sm">{content}</p>
                 </div>
             </div>
         </a>
