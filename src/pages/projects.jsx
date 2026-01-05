f701a7c364cab56f11ae62ec913cb9d564859bb0
Mr-Hariom-Kumar <766hariwork@gmail.com>
2026-01-05 22:10:03 +0530

    wrapped linkicon also in anchor tag

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -83,10 +83,12 @@ const Cards = () => {
               </Typography>
             </CardContent>
             <CardActions sx={{ justifyContent: 'center' }}>
-              <p className="relative z-10 mt-6 flex text-md font-semibold font-mono text-zinc-600 transition group-hover:text-[#00843D] dark:group-hover:text-yellow-400 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none scale-110" />
-                <a href={project.link.href}><span className="ml-2">{project.link.label}</span></a>
-              </p>
+              <a href={project.link.href} rel="noopener noreferrer">
+                <p className="relative z-10 mt-6 flex text-md font-semibold font-mono text-zinc-600 transition group-hover:text-[#00843D] dark:group-hover:text-yellow-400 dark:text-zinc-200">
+                  <LinkIcon className="h-6 w-6 flex-none scale-110" />
+                  <span className='ml-2'>{project.link.label}</span>  
+                </p>
+              </a>
             </CardActions>
           </MuiCard>
         </Grid>
