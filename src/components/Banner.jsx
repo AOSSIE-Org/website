cfb6b60fc87ea607ebc3ac87b1b13ca645d1e886
SMMuskan <mubashira.muskan.s.140@kalvium.community>
2026-03-08 15:38:38 +0530

    fixed the responsiveness of the Horizontal overflow and layout shift in hero section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,13 +7,13 @@ export function Banner() {
     <section aria-label="Apply Banner" className="scroll-mt-14 bg-[#00843D] dark:bg-yellow-400 sm:scroll-mt-32">
       <div className="overflow-hidden lg:relative">
         <ContainerPattern size="md" className="relative grid grid-cols-1 items-end gap-y-12 lg:static lg:grid-cols-2 pt-24 pb-8 sm:py-10">
-          <Pattern className="absolute -top-28 left-0 w-full sm:left-3/4 sm:-top-10 sm:ml-8 sm:w-auto md:left-2/3 lg:left-auto lg:right-2 lg:ml-0 xl:right-auto xl:left-2/3" />
-          <Pattern className="absolute mt-2 -top-32 left-0 w-full sm:left-3/4 sm:top-36 sm:ml-8 sm:w-auto md:left-2/3 lg:left-auto lg:right-2 lg:ml-0 xl:right-auto xl:left-2/3 sm:visible invisible" />
+          <Pattern className="absolute -top-28 left-0 w-full sm:left-[60%] sm:-top-10 sm:ml-8 sm:w-auto md:left-2/3 lg:left-auto lg:right-2 lg:ml-0 xl:right-auto xl:left-2/3" />
+          <Pattern className="absolute mt-2 -top-32 left-0 w-full sm:left-[60%] sm:top-36 sm:ml-8 sm:w-auto md:left-2/3 lg:left-auto lg:right-2 lg:ml-0 xl:right-auto xl:left-2/3 sm:visible invisible" />
           <div>
-            <h2 className="font-mono text-5xl font-black tracking-tighter text-white dark:text-black sm:w-3/4 sm:text-5xl md:w-2/3 lg:w-auto">
+            <h2 className="font-mono text-5xl font-black tracking-tighter text-white dark:text-black sm:w-2/5 md:w-2/3 lg:w-auto sm:text-5xl">
               Launch into AOSSIE&apos;s open-source world through GSoC!
             </h2>
-            <p className="mt-4 text-lg tracking-tighter text-white dark:text-black font-medium font-mono">
+            <p className="mt-4 text-lg tracking-tighter text-white dark:text-black font-medium font-mono sm:w-2/5 md:w-2/3">
               Learn how to apply for an opportunity to work on open-source projects and gain real-world experience through Google Summer of Code.
             </p>
             <div className="mt-5">
