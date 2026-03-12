c0427c177e5bfde60a66ca26c46053342fb932b4
SMMuskan <mubashira.muskan.s.140@kalvium.community>
2026-03-12 10:24:34 +0530

    Fix homepage logo scroll to top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -364,7 +364,7 @@ export function Header() {
               <div className="flex flex-1">
                   <HomeContainer>
                     <Home />
-                  </HomeContainer>git commit -m "Resolve merge conflicts with main"
+                  </HomeContainer>
               </div>
               <div className="flex flex-1 justify-end md:justify-center">
                 <MobileNavigation className="pointer-events-auto md:hidden" />
